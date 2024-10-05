e42b14aebc91c91e87c6a6ebae3c20311c841214
GustavoAssisDuran <148270241+GustavoAssisDuran@users.noreply.github.com>
2024-10-05 14:03:36 +0000

    adicionando-primeira-pagina-php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>999</title>
+</head>
+<body>
+    <?php
+    
+        $numero1 = 25;
+        $numero1 = 60;
+
+        $soma = $numero1 = 25 +         $numero1 = 60;
+
+
+        $soma = $numero1 = 25 +         $numero1 = 60;
+     <p>A soma de 25 e 60 é: <?php echo $soma ?> </p>
+    
+    ?>
+</body>
+</html>
